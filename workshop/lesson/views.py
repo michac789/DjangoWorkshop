b181843c626f4b599cf75ddfd7d3dde2ce3f9283
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-09-27 16:49:58 +0800

    03: Conditional Template Tag

--- workshop/lesson/views.py
+++ workshop/lesson/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
 
+valid_lessons = [
+    "math", "english", "physics"
+]
+
+
 def index(request):
     return render(request, "lesson/index.html")
 
 
 def view_lesson(request, lesson_name):
-    # print(lesson_name)
     return render(request, "lesson/view_lesson.html", {
         "title": lesson_name.capitalize(),
+        "valid": lesson_name in valid_lessons,
     })
