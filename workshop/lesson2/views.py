6e3b68ea4391434837bd32f1bc5b7b8e8facde27
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-09-29 17:32:00 +0800

    17: Displaying Models in Templates

--- workshop/lesson2/views.py
+++ workshop/lesson2/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Lesson, Student
 
 
 def index(request):
-    return render(request, "lesson2/index.html")
+    return render(request, "lesson2/index.html", {
+        "lessons": Lesson.objects.all(),
+        "students": Student.objects.all()
+    })
